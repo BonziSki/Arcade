b7bf22ea4f1587344b4ba14c6f712b4feeee234b
NOKIMIMO <janikowski.nicolas@gmail.com>
2023-01-11 15:03:03 +0100

    added menu
    Need correction
    -enter button not work
    -texture not work
    
    color higlights work

--- menu/menu.c
+++ menu/menu.c
@@ -2,20 +2,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "../func.h"
+#include "menu.h"
+
 void menu(SDL_Window * window, SDL_Renderer * renderer){
+
     SDL_bool quit = SDL_FALSE;
-    int choice = 1;
+    int choice = 0;
+
     while (!quit){
-        drawMenu(window,renderer,0);
         SDL_Event event;
+        drawMenu(window,renderer,choice);
         while (SDL_PollEvent(&event)){
+            
             switch (event.type)
             {
             case SDL_KEYDOWN:
                 switch (event.key.keysym.sym)
                 {
                     case SDLK_RIGHT:
-                        if (choice==4)
+                        if (choice==3)
                         {}else{
                             choice++;
                         }
@@ -30,10 +36,12 @@ void menu(SDL_Window * window, SDL_Renderer * renderer){
                         //-1 choix menu
                         break;
                     case SDLK_KP_ENTER:
+                            SDL_ClearScreen(renderer);
+                            SDL_Delay(10);
                         switch (choice)
                         {
                             case 1:
-                                //func(game)
+                                    
                                 break;
                             case 2:
                                 //func(game)
@@ -65,8 +73,47 @@ void menu(SDL_Window * window, SDL_Renderer * renderer){
 
 
 void drawMenu(SDL_Window * window, SDL_Renderer * renderer,int choice){
-    clean(window);
-    //dessin
-    //...drawmenu
-    
+    SDL_Surface * Surface1 = SDL_LoadBMP("./ressources/textures/choupette_block.bmp");
+    SDL_Texture * texture = SDL_CreateTextureFromSurface(renderer,Surface1);
+    SDL_FreeSurface(Surface1);
+    SDL_ClearScreen(renderer);
+    SDL_Rect * textRect = malloc(sizeof(SDL_Rect));
+    SDL_Rect * rect = malloc(sizeof(SDL_Rect));
+    if (SDL_SetRenderDrawColor(renderer, 255, 0,0, SDL_ALPHA_OPAQUE) != 0){
+        SDL_ExitWithError("Changement de couleur du rendu");
+    }
+    rect->w = 80;
+    rect->h = 80;
+    rect->y = 280;
+
+for (int i = 0; i < 4; i++)
+{
+    if (choice==i)
+    {
+        if (SDL_SetRenderDrawColor(renderer, 0, 0,255, SDL_ALPHA_OPAQUE) != 0){
+            SDL_ExitWithError("Changement de couleur du rendu");
+        }
+    }
+    rect->x = 40+ (80 + (rect->w*i*2));
+        SDL_RenderFillRect(renderer, rect);
+    if (SDL_SetRenderDrawColor(renderer, 255, 0,0, SDL_ALPHA_OPAQUE) != 0){
+        SDL_ExitWithError("Changement de couleur du rendu");
+    }
+}
+
+    rect->w = rect->w *3;
+    rect->h = 80;
+    rect->x = WIDTH/2-rect->w/2;
+    rect->y = 100;
+    SDL_RenderFillRect(renderer, rect);
+
+    textRect->w=80;
+    textRect->h=80;
+    textRect->x=300;
+    textRect->y=400;
+    SDL_RenderFillRect(renderer, textRect);
+
+    SDL_RenderPresent(renderer);
+    SDL_RenderCopy(renderer,texture,NULL,textRect);
+    //SDL_DestroyTexture(texture);
 }
